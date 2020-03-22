bb4e611bb9b3986dcfb41ab3cb039d82ab8145bf
misswmn <568890375@qq.com>
2020-03-22 14:25:42 +0800

    Initial commit

---
+++ src/main/java/com/arch/SpringArchApplication.java
@@ -0,0 +1,13 @@
+package com.arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringArchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/arch/SpringArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringArchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
